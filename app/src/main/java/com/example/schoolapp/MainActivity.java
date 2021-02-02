45f0102a74da14dda6dc598b5299a721c697bdc9
gopssays <gopinamgiri14@gmail.com>
2021-02-02 17:49:49 +0530

    hii

--- app/src/main/java/com/example/schoolapp/MainActivity.java
+++ app/src/main/java/com/example/schoolapp/MainActivity.java
@@ -21,75 +21,75 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private BottomNavigationView bottomNavigationView;
-    private NavController navController;
-Toolbar toolbar;
+        private BottomNavigationView bottomNavigationView;
+        private NavController navController;
+        Toolbar toolbar;
 
-    //drawer activity
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle toggle;
-    private NavigationView navigationView;
-    ActionBar actionBar;
+        //drawer activity
+        private DrawerLayout drawerLayout;
+        private ActionBarDrawerToggle toggle;
+        private NavigationView navigationView;
+        ActionBar actionBar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        bottomNavigationView=findViewById(R.id.bnv);
-        navController= Navigation.findNavController(this,R.id.navHostFragment);
+            bottomNavigationView=findViewById(R.id.bnv);
+            navController= Navigation.findNavController(this,R.id.navHostFragment);
 
 
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.navigation_view);
-        toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.start,R.string.close);
+            drawerLayout=findViewById(R.id.drawer_layout);
+            navigationView=findViewById(R.id.navigation_view);
+            toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.start,R.string.close);
 
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+            drawerLayout.addDrawerListener(toggle);
+            toggle.syncState();
 
 
 //        actionBar = getSupportActionBar();
 //        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-        // TODO: Remove the redundant calls to getSupportActionBar()
-        //       and use variable actionBar instead
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+            // TODO: Remove the redundant calls to getSupportActionBar()
+            //       and use variable actionBar instead
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
 
 
-        navigationView.setNavigationItemSelectedListener(this);
+            navigationView.setNavigationItemSelectedListener(this);
 
-        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+            NavigationUI.setupWithNavController(bottomNavigationView,navController);
 
 
-    }
+        }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (toggle.onOptionsItemSelected(item))
-        {
+        @Override
+        public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+            if (toggle.onOptionsItemSelected(item))
+            {
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId())
+            {
+                case R.id.navigation_video:
+                    Toast.makeText(this, "Video Selected", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.navigation_Gallery:
+                    Toast.makeText(this, "Gallery Selected", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.navigation_share:
+                    Toast.makeText(this, "Share Selected", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+
             return true;
         }
-        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId())
-        {
-            case R.id.navigation_video:
-                Toast.makeText(this, "Video Selected", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.navigation_Gallery:
-                Toast.makeText(this, "Gallery Selected", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.navigation_share:
-                Toast.makeText(this, "Share Selected", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        return true;
-    }
-}
